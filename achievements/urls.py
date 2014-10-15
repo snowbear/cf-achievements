d14ff609d11fd018b6a3bc5eb75f4e19e132bb4c
Marat Yuldashev <ymarat87@mail.ru>
2014-10-15 22:15:28 +0400

    couple of views to show last achievements in total and last achievements per user

--- achievements/urls.py
+++ achievements/urls.py
@@ -4,4 +4,5 @@ from achievements import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^profile/(?P<handle>.+)$', views.profile, name='profile'),
 )
--- achievements/views.py
+++ achievements/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from achievements.models import *
+
 def index(request):
-    return HttpResponse("Ach index")
+    last_achievements = Rewarding.objects.order_by("-date")[:20]
+    return render(request,
+                  'achievements/index.html', {
+                        'last_achievements': last_achievements,
+                  })
+
+def profile(request, handle):
+    contestant = Contestant.objects.get(handle = handle)
+    last_achievements = Rewarding.objects.filter(participant = contestant).order_by("-date")[:20]
+    return render(request,
+                  'achievements/profile.html', {
+                        'last_achievements': last_achievements,
+                  })
