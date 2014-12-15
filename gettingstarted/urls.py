2b8496bddc0ded232c95735300b93d705b0f2551
Marat Yuldashev <ymarat87@mail.ru>
2014-12-15 19:15:46 +0300

    removed hello/ folder as well

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,18 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'gettingstarted.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^', include('achievements.urls', namespace="achievements")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^achievements/', include('achievements.urls', namespace="achievements")),
     url(r'^data-management/', include('data_management.urls', namespace="data")),
-
 )
