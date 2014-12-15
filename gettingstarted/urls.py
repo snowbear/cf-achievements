9c3d83241b4885e525eb02d017c4f644846a433c
Marat Yuldashev <ymarat87@mail.ru>
2014-12-15 19:18:09 +0300

    removed duplicate 'achievemnts' url config

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -7,6 +7,5 @@ urlpatterns = patterns('',
     url(r'^', include('achievements.urls', namespace="achievements")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('polls.urls', namespace="polls")),
-    url(r'^achievements/', include('achievements.urls', namespace="achievements")),
     url(r'^data-management/', include('data_management.urls', namespace="data")),
 )
