5be9d5c5561d359b09bec975c7ee53078ed144e1
Marat Yuldashev <marat.snowbear@gmail.com>
2017-02-19 02:16:56 +0200

    PEP style warnings cleanup

--- source/achievements/models.py
+++ source/achievements/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
+
 class Contestant(models.Model):
-    handle = models.CharField(max_length=200, unique = True)
-    rating = models.IntegerField(null = True)
+    handle = models.CharField(max_length=200, unique=True)
+    rating = models.IntegerField(null=True)
 
     def __str__(self):
         return self.handle
 
+
 class Contest(models.Model):
-    id = models.IntegerField(primary_key = True)
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     date = models.DateTimeField()
     order = models.IntegerField()
@@ -16,21 +18,23 @@ class Contest(models.Model):
     def __str__(self):
         return self.name
 
+
 class Achievement(models.Model):
-    id = models.IntegerField(primary_key = True)
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.name
 
+
 class Rewarding(models.Model):
     participant = models.ForeignKey(Contestant)
     achievement = models.ForeignKey(Achievement)
     comment = models.CharField(max_length=1000)
     date = models.DateTimeField()
-    contest = models.ForeignKey(Contest, null = True)
-    level = models.IntegerField(null = True)
+    contest = models.ForeignKey(Contest, null=True)
+    level = models.IntegerField(null=True)
 
     def __str__(self):
         return self.participant.handle + " - " + self.achievement.name
--- source/helpers/sql.py
+++ source/helpers/sql.py
@@ -1,7 +1,8 @@
 from django.db import *
 import logging
 
-def execute_sql(sql, params = []):
+
+def execute_sql(sql, params=[]):
     cursor = connection.cursor()
     cursor.execute(sql, params)
     if sql.strip()[:10].lower().startswith('select'):
@@ -10,24 +11,29 @@ def execute_sql(sql, params = []):
         logging.info("affected %d rows", cursor.rowcount)
         return cursor.rowcount
 
+
 def batch_insert(table_name, column_names, values_list):
     assert(type(column_names) == tuple)
-    if len(values_list) == 0: return
+    if len(values_list) == 0:
+        return
     
     values_string = ",".join(str(val) for val in values_list)
     query = '''
             INSERT INTO %s (%s)
             VALUES
             %s
-            ''' % (table_name , ",".join(column_names) , values_string)
+            ''' % (table_name, ",".join(column_names), values_string)
     execute_sql(query)
+
 
 def batch_update(table_name, filter_column_name, update_column_name, data):
     assert(type(data) == list)
     if len(data) == 0: return
     
-    logging.info("Updating {table_name}.{update_column_name}, filtering by {filter_column_name}. {n} items are supposed to be updated"
-                    .format(table_name = table_name, update_column_name = update_column_name, filter_column_name = filter_column_name, n = len(data)))
+    logging.info("Updating {table_name}.{update_column_name}, "
+                 "filtering by {filter_column_name}. {n} items are supposed to be updated"
+                 .format(table_name=table_name, update_column_name=update_column_name,
+                         filter_column_name=filter_column_name, n=len(data)))
     for t in data: 
         assert(type(t) == tuple)
         assert(len(t) == 2)
@@ -42,9 +48,9 @@ def batch_update(table_name, filter_column_name, update_column_name, data):
         WHERE c.{filter_column_name} = s.c1
     """.format(
             table_name = table_name,
-            filter_column_name = filter_column_name,
-            update_column_name = update_column_name,
-            data_string = ",".join(["('{c1}',{c2})".format(c1 = h[0], c2 = h[1]) for h in data])
+            filter_column_name=filter_column_name,
+            update_column_name=update_column_name,
+            data_string=",".join(["('{c1}',{c2})".format(c1=h[0], c2=h[1]) for h in data])
         )
 
     return execute_sql(query)
